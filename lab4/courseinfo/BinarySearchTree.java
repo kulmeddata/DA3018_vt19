12e1ee9dad38593c78e2f9d31cc95760645f996f
MyGitUsername <48906260+kulmeddata@users.noreply.github.com>
2019-05-06 15:28:02 +0200

    Add Iterator stuff, testing and app-functionality

--- lab4/courseinfo/BinarySearchTree.java
+++ lab4/courseinfo/BinarySearchTree.java
@@ -1,62 +1,64 @@
 package courseinfo;
 
 import java.util.NoSuchElementException;
+import java.util.Iterator;
+import java.util.Stack;
 
 /**
  * Store course information in a binary search tree
  *
  */
-public class BinarySearchTree {
-	BSTNode root=null;
+public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
+    BSTNode root=null;
 
-	public BinarySearchTree() {
-		// Empty constructor?
-	}
+    public BinarySearchTree() {
+        // Empty constructor?
+    }
 
-	/**
-	 * Public interface for inserting data into the datastructure. Utilizes
-	 * a private, more technical method.
-	 * @param courseCode, eg "DA3018"
-	 * @param courseName, eg "Computer Science"
-	 * @param courseCredits, eg 7,5
-	 */
-	public void insert(String courseCode, String courseName, double courseCredits) {
-		BSTNode node = new BSTNode(courseCode, courseName, courseCredits);
-		root = insert(root, node);
-	}
+    /**
+     * Public interface for inserting data into the datastructure. Utilizes
+     * a private, more technical method.
+     * @param courseCode, eg "DA3018"
+     * @param courseName, eg "Computer Science"
+     * @param courseCredits, eg 7,5
+     */
+    public void insert(String courseCode, String courseName, double courseCredits) {
+        BSTNode node = new BSTNode(courseCode, courseName, courseCredits);
+        root = insert(root, node);
+    }
 
-	/**
-	 * Insert 'node' into the tree pointed at by 'root'.
-	 * @returns The node that should be the root of this subtree.
-	 * @param root
-	 * @param node
-	 */
-	private BSTNode insert(BSTNode root, BSTNode node) {
-		if (root==null) {
-			return node; // The easy case. Let the current node be the root of a new (sub?)tree.
-		} else {
-			String currentKey = root.getCourseCode();
-			BSTNode left = root.getLeftChild();
-			BSTNode right = root.getRightChild();
-			if (node.getCourseCode().compareTo(currentKey) < 0) { // left string "before" right string
-				left = insert(left, node);
-			} else if (node.getCourseCode().compareTo(currentKey) > 0) { // left string "after" right string
-				right = insert(right, node);
-			} else { // if key already in tree, overwrite corresponding data
+    /**
+     * Insert 'node' into the tree pointed at by 'root'.
+     * @returns The node that should be the root of this subtree.
+     * @param root
+     * @param node
+     */
+    private BSTNode insert(BSTNode root, BSTNode node) {
+        if (root==null) {
+            return node; // The easy case. Let the current node be the root of a new (sub?)tree.
+        } else {
+            String currentKey = root.getCourseCode();
+            BSTNode left = root.getLeftChild();
+            BSTNode right = root.getRightChild();
+            if (node.getCourseCode().compareTo(currentKey) < 0) { // left string "before" right string
+                left = insert(left, node);
+            } else if (node.getCourseCode().compareTo(currentKey) > 0) { // left string "after" right string
+                right = insert(right, node);
+            } else { // if key already in tree, overwrite corresponding data
                 node.setChildren(root.getLeftChild(), root.getRightChild());
                 return node;
             }
 
-			root.setChildren(left, right);
-			return root;
-		}
-	}
+            root.setChildren(left, right);
+            return root;
+        }
+    }
 
-	/**
-	 * size: Count the number of nodes in the search tree
-	 */
+    /**
+     * size: Count the number of nodes in the search tree
+     */
 
-	public int size() {
+    public int size() {
         return size(root);
     }
     private int size(BSTNode root) {
@@ -66,10 +68,10 @@ public class BinarySearchTree {
         return 1 + size(root.getLeftChild()) + size(root.getRightChild());
     }
 
-	/**
-	 * find: Find a course given a course code
-	 */
-	public BSTNode find(String courseCode) throws NoSuchElementException {
+    /**
+     * find: Find a course given a course code
+     */
+    public BSTNode find(String courseCode) throws NoSuchElementException {
         BSTNode node = find(root, courseCode);
         if (node==null) {
             throw new NoSuchElementException("Given key not in BST!");
@@ -89,7 +91,7 @@ public class BinarySearchTree {
         return root;
     }
 
-	/**
+    /**
      * Public interface for removing data from the data-structure. Utilizes
      * a private, more technical method.
      * @param courseCode, eg "DA3018"
@@ -139,8 +141,8 @@ public class BinarySearchTree {
             return root;
         }
         return min(root.getLeftChild());
-	}
-	/**
+    }
+    /**
      * Print the data of each node using in-order-traversal of BST.
      */
     public void inOrderPrint() {
@@ -155,59 +157,99 @@ public class BinarySearchTree {
         inOrderPrint(root.getRightChild());
     }
 
-	/**
-	 * Nodes in the search tree
-	 * This class should be sufficient for the project.
-	 *
-	 */
-	public class BSTNode {
-		private String courseCode;
-		private String courseName;
-		private double credits;
-		private BSTNode left = null;
-		private BSTNode right = null;
+    public Iterator<BinarySearchTree.BSTNode> iterator() {
+        return new BSTIterator(root);
+    }
 
-		/**
-		 * Constructor
-		 *
-		 */
-		public BSTNode(String code, String name, double credits) {
-			this.courseCode = code;
-			this.courseName = name;
-			this.credits = credits;
-		}
+    private class BSTIterator implements Iterator<BinarySearchTree.BSTNode> {
 
-		/**
-		 * Specify the children of the current node
-		 * @param left
-		 * @param right
-		 */
-		public void setChildren(BSTNode left, BSTNode right) {
-			this.left = left;
-			this.right = right;
-		}
+        private Stack<BSTNode> stack; // top of stack will contain leftmost/min node
 
-		public String getCourseCode() {
-			return courseCode;
-		}
+        private BSTIterator(BSTNode root) {
+            stack = new Stack<>();
+            pushLeft(root);
+        }
 
-		public String getCourseName() {
-			return courseName;
-		}
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
 
-		public double getCredits() {
-			return credits;
-		}
+        public BSTNode next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("No further in-order-successor-node in BST!");
+            }
+            BSTNode next = stack.pop();
+            pushLeft(next.getRightChild()); // descendants of right child are next in line
+            return next;
+        }
 
-		public BSTNode getLeftChild() {
-			return left;
-		}
+        public void remove() {
+            throw new UnsupportedOperationException("Operation not supported in current context!");
+        }
 
-		public BSTNode getRightChild() {
-			return right;
-		}
+        private void pushLeft(BSTNode node) {
+            // from node to its leftmost descendant, push to stack
+            while (node != null) {
+                stack.push(node);
+                node = node.getLeftChild();
+            }
+        }
+
+    }
+
+    /**
+     * Nodes in the search tree
+     * This class should be sufficient for the project.
+     *
+     */
+    public class BSTNode {
+        private String courseCode;
+        private String courseName;
+        private double credits;
+        private BSTNode left = null;
+        private BSTNode right = null;
+
+        /**
+         * Constructor
+         *
+         */
+        public BSTNode(String code, String name, double credits) {
+            this.courseCode = code;
+            this.courseName = name;
+            this.credits = credits;
+        }
+
+        /**
+         * Specify the children of the current node
+         * @param left
+         * @param right
+         */
+        public void setChildren(BSTNode left, BSTNode right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        public String getCourseCode() {
+            return courseCode;
+        }
+
+        public String getCourseName() {
+            return courseName;
+        }
+
+        public double getCredits() {
+            return credits;
+        }
+
+        public BSTNode getLeftChild() {
+            return left;
+        }
+
+        public BSTNode getRightChild() {
+            return right;
+        }
 
 
 
-	}
+    }
 }
--- lab4/courseinfo/CourseInfo.java
+++ lab4/courseinfo/CourseInfo.java
@@ -1,19 +1,117 @@
 package courseinfo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CourseInfo {
 
-	public static void main(String[] args) {
-		BinarySearchTree courses = new BinarySearchTree();
-		
-		courses.insert("DA3018", "Computer Science", 7.5);
-		courses.insert("MM2001", "Matematik I", 30.0);
-		courses.insert("DA2004", "Programmeringsteknik", 7.5);
-		
-		int n = courses.size();
-		System.out.printf("There are %d courses in the database.\n", n);
-		
-		String name = courses.find("MM2001").getCourseName();
-		System.out.printf("Name: %s\n", name);
-	}
+    public static void main(String[] args) {
+        BinarySearchTree courses = new BinarySearchTree();
+
+//        courses.insert("DA3018", "Computer Science", 7.5);
+//        courses.insert("MM2001", "Matematik I", 30.0);
+//        courses.insert("DA2004", "Programmeringsteknik", 7.5);
+//
+//        int n = courses.size();
+//        System.out.printf("There are %d courses in the database.\n", n);
+//
+//        String name = null;
+//        try {
+//            name = courses.find("MM2001").getCourseName();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        System.out.printf("Name: %s\n", name);
+//
+//        System.out.println();
+//        System.out.println("New tests");
+//        System.out.println();
+//        courses.inOrderPrint();
+//        System.out.println("Size = " + courses.size());
+//        System.out.println();
+//        courses.insert("ABC123", "Course1", 7.5);
+//        courses.insert("CCB111", "Course2", 7.5);
+//        courses.insert("BBB213", "Course3", 7.5);
+//        courses.insert("BAB112", "Course4", 7.5);
+//        courses.insert("CCC222", "Course5", 7.5);
+//        courses.insert("AAC333", "Course6", 7.5);
+//        courses.insert("AAC345", "Course7", 7.5);
+//        courses.insert("BAA345", "Course8", 7.5);
+//        courses.insert("AAA111", "Course9", 7.5);
+//        courses.insert("AAA121", "Course10", 7.5);
+//        courses.insert("AAA233", "Course11", 7.5);
+//        courses.insert("AAA231", "Course12", 7.5);
+//        courses.insert("CCC323", "Course13", 7.5);
+//        courses.insert("CCE500", "Course14", 7.5);
+//        courses.insert("CCC211", "Course15", 7.5);
+//        courses.insert("XYZ100", "Course16", 7.5);
+//        courses.inOrderPrint();
+//        System.out.println("Size = " + courses.size());
+//        System.out.println();
+//        System.out.println("Overwrite AAC333");
+//        courses.insert("AAC333", "Course600", 70.5);
+//        courses.inOrderPrint();
+//        System.out.println("Size = " + courses.size());
+//        System.out.println();
+//        System.out.println("Remove CCE500");
+//        try {
+//            courses.remove("CCE500");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        courses.inOrderPrint();
+//        System.out.println("Size = " + courses.size());
+//        System.out.println();
+//        System.out.println("Remove AAA121");
+//        try {
+//            courses.remove("AAA121");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        courses.inOrderPrint();
+//        System.out.println("Size = " + courses.size());
+//        System.out.println();
+//        System.out.println("Remove CCB111");
+//        try {
+//            courses.remove("CCB111");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        courses.inOrderPrint();
+//        System.out.println("Size = " + courses.size());
+//        System.out.println();
+//        System.out.println("Test iterator");
+//        for (BinarySearchTree.BSTNode node: courses) {
+//            System.out.println(node.getCourseCode());
+//        }
+
+        // read data into BST
+        try (Scanner fileScanner = new Scanner(new File(args[0]))) {
+            while (fileScanner.hasNextLine()) {
+                String[] line = fileScanner.nextLine().split(",");
+                String courseCode = line[0];
+                String courseName = line[1];
+                double credits  = Double.parseDouble(line[2]);
+                courses.insert(courseCode, courseName, credits);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        // list data for single key
+        if (args.length > 1) {
+            String courseCode = args[1];
+            BinarySearchTree.BSTNode node = courses.find(courseCode);
+            System.out.println(node.getCourseCode() + "\t" + node.getCourseName() + "\t\t" + node.getCredits());
+            return;
+        }
+        // list data for each node in BST, sorted by key
+        double sum = 0;
+        for (BinarySearchTree.BSTNode node: courses) {
+            System.out.println(node.getCourseCode() + "\t" + node.getCourseName() + "\t\t" + node.getCredits());
+            sum += node.getCredits();
+        }
+        System.out.println("Summa" + "\t\t\t\t" + sum);
+    }
 
 }
